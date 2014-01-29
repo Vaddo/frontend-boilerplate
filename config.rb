4c2335fc70c010b10ddb461da33de0a74b32e7f7
Vadim Hermann <vhermann@wheregroup.com>
2014-01-29 14:27:53 +0100

    arrow mixin

--- config.rb
+++ config.rb
@@ -6,9 +6,10 @@ css_dir = "styles"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "scripts"
+sass_options = { :debug_info => true }
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
